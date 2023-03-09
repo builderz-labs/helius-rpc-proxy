561490e903663ea7404fdb308580061f8700522e
Ruben Schaller <rubenusshd@gmail.com>
2023-03-09 18:49:09 +0100

    devnet

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ interface Env {
 
 export default {
 	async fetch(request: Request, env: Env) {
-
 		// If the request is an OPTIONS request, return a 200 response with permissive CORS headers
 		// This is required for the Helius RPC Proxy to work from the browser and arbitrary origins
 		// If you wish to restrict the origins that can access your Helius RPC Proxy, you can do so by
@@ -13,34 +12,39 @@ export default {
 		// For example, if you wanted to allow requests from `https://example.com`, you would change the
 		// header to `https://example.com`.
 		const corsHeaders = {
-			"Access-Control-Allow-Origin": `${env.CORS_ALLOW_ORIGIN || '*'}`,
-			"Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, OPTIONS",
-			"Access-Control-Allow-Headers": "*",
-		}
+			'Access-Control-Allow-Origin': `${env.CORS_ALLOW_ORIGIN || '*'}`,
+			'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, OPTIONS',
+			'Access-Control-Allow-Headers': '*',
+		};
 
-		if (request.method === "OPTIONS") {
+		if (request.method === 'OPTIONS') {
 			return new Response(null, {
 				status: 200,
 				headers: corsHeaders,
 			});
 		}
 
-		const upgradeHeader = request.headers.get('Upgrade')
-    if (upgradeHeader || upgradeHeader === 'websocket') {
-      return await fetch(`https://rpc.helius.xyz/?api-key=${env.HELIUS_API_KEY}`, request)
-    }
+		const upgradeHeader = request.headers.get('Upgrade');
+		if (upgradeHeader || upgradeHeader === 'websocket') {
+			return await fetch(`https://rpc-devnet.helius.xyz/?api-key=${env.HELIUS_API_KEY}`, request);
+		}
 
-		const {pathname, search} = new URL(request.url)
+		const { pathname, search } = new URL(request.url);
 		const payload = await request.text();
-		const proxyRequest = new Request(`https://${pathname === '/' ? 'rpc' : 'api'}.helius.xyz${pathname}?api-key=${env.HELIUS_API_KEY}${search ? `&${search.slice(1)}` : ''}`, {
-			method: request.method,
-			body: payload || null,
-			headers: {
-				'Content-Type': 'application/json',
-				'X-Helius-Cloudflare-Proxy': 'true',
-				...corsHeaders,
+		const proxyRequest = new Request(
+			`https://${pathname === '/' ? 'rpc-devnet' : 'api'}.helius.xyz${pathname}?api-key=${
+				env.HELIUS_API_KEY
+			}${search ? `&${search.slice(1)}` : ''}`,
+			{
+				method: request.method,
+				body: payload || null,
+				headers: {
+					'Content-Type': 'application/json',
+					'X-Helius-Cloudflare-Proxy': 'true',
+					...corsHeaders,
+				},
 			}
-		});
+		);
 
 		return await fetch(proxyRequest);
 	},
